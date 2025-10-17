5342921e4e100c0addc8c525214502b8be377c09
Edgar <edr9719@outlook.com>
2025-10-17 17:22:06 -0600

    Tare de logica de programacion

---
+++ script.js
@@ -0,0 +1,45 @@
+const input = document.getElementById('input');
+const identifier = document.getElementById('identifier');
+const highestToLowest = document.getElementById('highest-to-lowest');
+const lowestToHighest = document.getElementById('lowest-to-highest');
+const sameNumbers = document.getElementById('same-numbers');
+const button = document.getElementById('button');
+let numbers;
+
+function clearDOM() {
+  identifier.innerHTML = '';
+  highestToLowest.innerHTML = '';
+  lowestToHighest.innerHTML = '';
+  sameNumbers.innerHTML = '';
+}
+
+button.addEventListener('click', () => {
+  clearDOM();
+
+  numbers = input.value;
+  numbers = numbers.split('').map(Number);
+
+  if (checkSameNumbers(numbers)) {
+    sameNumbers.innerHTML += `All the numbers are the same`;
+  } else {
+    highestCenterMinor(numbers);
+    descendingAscending(numbers);
+  }
+});
+
+function highestCenterMinor(numbers) {
+  const max = Math.max(...numbers);
+  const min = Math.min(...numbers);
+  identifier.innerHTML += `The highest number is ${max}, the lowest is ${min} and the one in the center is ${numbers[1]}`;
+}
+
+function descendingAscending(numbers) {
+  const ascending = numbers.sort().join('');
+  const descending = numbers.sort().reverse().join('');
+  highestToLowest.innerHTML += `Lowest to Highest order: ${ascending}<br/>Highest to lowest order: ${descending}`;
+}
+
+function checkSameNumbers(numbers) {
+  const allSame = numbers.every((num) => num === numbers[0]);
+  return allSame;
+}
